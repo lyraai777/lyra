ec75f29905f1919f513288c8ece5d7a9edd150a7
lyraai777 <ananthus1221@gmail.com>
2026-01-23 20:56:21 +0530

    Delete lyra directory

--- lyra/js/scroll.js
+++
@@ -1,6 +0,0 @@
-export function createScrollContainer(content) {
-  const container = document.createElement("div");
-  container.className = "scroll-container";
-  container.appendChild(content);
-  return container;
-}
